3255457dc72ff63cf2f1cdd56ddb1f0ab2caae5f
Hauglid, Thomas <karl.thomas.hauglid@storebrand.no>
2018-12-11 18:38:51 +0100

    Pick image from album or camera. Keyboard will hide/show handled.

---
+++ MemeMe/TextFieldDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  TextFieldDelegate.swift
+//  MemeMe
+//
+//  Created by Thomas Hauglid on 11/12/2018.
+//  Copyright © 2018 Thomas Hauglid. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+
+class TextFieldDelegate : NSObject, UITextFieldDelegate {
+    
+    override init() {
+        super.init()
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "TOP" || textField.text == "BOTTOM" {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,13 +8,134 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
+
+    // Outlets
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var albumButton: UIBarButtonItem!
+    
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
+    
+    var textFieldDelegate = TextFieldDelegate()
+    
+    //Lifecycle funcs
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unSubscribeToKeyboardNotifications()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        initTextField(topText)
+        initTextField(bottomText)
     }
 
-
+    // Init stuffs?
+    func initTextField(_ textField: UITextField){
+        textField.delegate = textFieldDelegate
+        let memeTextAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor:UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.strokeWidth:  -2
+        ]
+        textField.defaultTextAttributes = memeTextAttributes
+        textField.textAlignment = .center
+    }
+    
+    // ###### Save an image stuff ######
+    func save() {
+        // Create the meme
+        let meme = Meme(topText: topText.text!, bottomText: bottomText.text!, originalImage: imageView.image!, memedImage: generateMemedImage())
+    }
+    
+    func generateMemedImage() -> UIImage {
+        
+        // TODO: Hide toolbar and navbar
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        // TODO: Show toolbar and navbar
+        
+        return memedImage
+    }
+    
+    // ###### Pick an image stuff ######
+        @IBAction func pickAnImage(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        let button = sender as! UIBarButtonItem
+        
+        imagePicker.delegate = self
+        
+        if button.isEqual(cameraButton) {
+            imagePicker.sourceType = .camera
+        }else {
+            imagePicker.sourceType = .photoLibrary
+        }
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        if let image = info[.originalImage] as? UIImage {
+            imageView.image = image
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_: UIImagePickerController){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    // ###### Keyboard stuff ######
+    @objc func keyboardWillShow(_ notification: Notification){
+        view.frame.origin.y = -getKeyboardHeight(notification)
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification){
+        view.frame.origin.y = 0
+    }
+    
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    func unSubscribeToKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat{
+        let userinfo = notification.userInfo
+        let keyboardSize = userinfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
 }
 
